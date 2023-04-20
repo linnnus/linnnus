18a8cbea2a16acb39f3c88f8e0acaadddbb05e33
linnnus <linnnus@users.noreply.github.com>
2023-04-20 10:37:09 +0200

    Update readme builder beep beppbepp

--- build_readme.py
+++ build_readme.py
@@ -5,5 +5,5 @@ with open("README.md", "wt") as f:
     print("# Linus\n", file=f)
     print("That's me.\n", file=f)
     print("## Meta info\n", file=f)
-    print(f"Python version: {platform.python_version()}", file=f)
+    print(f"Python version: {platform.python_version()}\n", file=f)
     print(f"Date: {datetime.datetime.now()}", file=f)
