b958eab2fec87339e37cf864fee782c1bacef72d
linnnus <linnnus@users.noreply.github.com>
2023-04-20 09:44:07 +0200

    Initial commit

---
+++ build_readme.py
@@ -0,0 +1,9 @@
+import datetime
+import platform
+
+with open("README.md", "wt") as f:
+    print("# Linus\n", file=f)
+    print("That's me.\n", file=f)
+    print("## Meta info\n", file=f)
+    print(f"Python version: {platform.python_version()}", file=f)
+    print(f"Date: {datetime.datetime.now()}", file=f)
